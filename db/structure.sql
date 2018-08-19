b05e19c8ef91844e0029ec8217dff62202697b25
Derek Shiller <dcshiller@gmail.com>
2018-08-19 08:37:05 -0400

    update schema

--- db/structure.sql
+++ db/structure.sql
@@ -1,11 +1,10 @@
 SET statement_timeout = 0;
 SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
 SET client_min_messages = warning;
-SET row_security = off;
 
 --
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
@@ -21,17 +20,15 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
-SET search_path = public, pg_catalog;
-
 SET default_tablespace = '';
 
 SET default_with_oids = false;
 
 --
--- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -
+-- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
-CREATE TABLE ar_internal_metadata (
+CREATE TABLE public.ar_internal_metadata (
     key character varying NOT NULL,
     value character varying,
     created_at timestamp without time zone NOT NULL,
@@ -40,10 +37,10 @@ CREATE TABLE ar_internal_metadata (
 
 
 --
--- Name: articles; Type: TABLE; Schema: public; Owner: -
+-- Name: articles; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
-CREATE TABLE articles (
+CREATE TABLE public.articles (
     id bigint NOT NULL,
     user_id bigint,
     title character varying,
@@ -56,7 +53,7 @@ CREATE TABLE articles (
 -- Name: articles_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
-CREATE SEQUENCE articles_id_seq
+CREATE SEQUENCE public.articles_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -68,14 +65,14 @@ CREATE SEQUENCE articles_id_seq
 -- Name: articles_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
-ALTER SEQUENCE articles_id_seq OWNED BY articles.id;
+ALTER SEQUENCE public.articles_id_seq OWNED BY public.articles.id;
 
 
 --
--- Name: documents; Type: TABLE; Schema: public; Owner: -
+-- Name: documents; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
-CREATE TABLE documents (
+CREATE TABLE public.documents (
     id bigint NOT NULL,
     file character varying,
     name character varying,
@@ -90,7 +87,7 @@ CREATE TABLE documents (
 -- Name: documents_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
-CREATE SEQUENCE documents_id_seq
+CREATE SEQUENCE public.documents_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -102,14 +99,14 @@ CREATE SEQUENCE documents_id_seq
 -- Name: documents_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
-ALTER SEQUENCE documents_id_seq OWNED BY documents.id;
+ALTER SEQUENCE public.documents_id_seq OWNED BY public.documents.id;
 
 
 --
--- Name: journals; Type: TABLE; Schema: public; Owner: -
+-- Name: journals; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
-CREATE TABLE journals (
+CREATE TABLE public.journals (
     id bigint NOT NULL,
     title character varying,
     created_at timestamp without time zone NOT NULL,
@@ -121,7 +118,7 @@ CREATE TABLE journals (
 -- Name: journals_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
-CREATE SEQUENCE journals_id_seq
+CREATE SEQUENCE public.journals_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -133,14 +130,14 @@ CREATE SEQUENCE journals_id_seq
 -- Name: journals_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
-ALTER SEQUENCE journals_id_seq OWNED BY journals.id;
+ALTER SEQUENCE public.journals_id_seq OWNED BY public.journals.id;
 
 
 --
--- Name: submission_events; Type: TABLE; Schema: public; Owner: -
+-- Name: submission_events; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
-CREATE TABLE submission_events (
+CREATE TABLE public.submission_events (
     id bigint NOT NULL,
     submission_id bigint,
     event_type character varying,
@@ -152,10 +149,10 @@ CREATE TABLE submission_events (
 
 
 --
--- Name: submissions; Type: TABLE; Schema: public; Owner: -
+-- Name: submissions; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
-CREATE TABLE submissions (
+CREATE TABLE public.submissions (
     id bigint NOT NULL,
     article_id bigint,
     journal_id bigint,
@@ -168,13 +165,13 @@ CREATE TABLE submissions (
 -- Name: latest_article_events_by_users; Type: VIEW; Schema: public; Owner: -
 --
 
-CREATE VIEW latest_article_events_by_users AS
+CREATE VIEW public.latest_article_events_by_users AS
  SELECT DISTINCT ON (articles.id) articles.id AS article_id,
     articles.user_id,
     submission_events.date AS latest_date
-   FROM ((articles
-     JOIN submissions ON ((articles.id = submissions.article_id)))
-     JOIN submission_events ON ((submissions.id = submission_events.submission_id)))
+   FROM ((public.articles
+     JOIN public.submissions ON ((articles.id = submissions.article_id)))
+     JOIN public.submission_events ON ((submissions.id = submission_events.submission_id)))
   ORDER BY articles.id, submission_events.date DESC;
 
 
@@ -182,14 +179,14 @@ CREATE VIEW latest_article_events_by_users AS
 -- Name: latest_journal_events_by_users; Type: VIEW; Schema: public; Owner: -
 --
 
-CREATE VIEW latest_journal_events_by_users AS
+CREATE VIEW public.latest_journal_events_by_users AS
  SELECT DISTINCT ON (journals.id) journals.id AS journal_id,
     articles.user_id,
     submission_events.date AS latest_date
-   FROM (((journals
-     JOIN submissions ON ((journals.id = submissions.journal_id)))
-     JOIN articles ON ((articles.id = submissions.article_id)))
-     JOIN submission_events ON ((submissions.id = submission_events.submission_id)))
+   FROM (((public.journals
+     JOIN public.submissions ON ((journals.id = submissions.journal_id)))
+     JOIN public.articles ON ((articles.id = submissions.article_id)))
+     JOIN public.submission_events ON ((submissions.id = submission_events.submission_id)))
   ORDER BY journals.id, submission_events.date DESC;
 
 
@@ -197,22 +194,22 @@ CREATE VIEW latest_journal_events_by_users AS
 -- Name: latest_submission_events_by_submissions; Type: VIEW; Schema: public; Owner: -
 --
 
-CREATE VIEW latest_submission_events_by_submissions AS
+CREATE VIEW public.latest_submission_events_by_submissions AS
  SELECT DISTINCT ON (submissions.id) submissions.article_id,
     submissions.id AS submission_id,
     submissions.journal_id,
     submission_events.date AS latest_date,
     submission_events.event_subtype
-   FROM (submissions
-     JOIN submission_events ON ((submissions.id = submission_events.submission_id)))
+   FROM (public.submissions
+     JOIN public.submission_events ON ((submissions.id = submission_events.submission_id)))
   ORDER BY submissions.id, submission_events.date DESC;
 
 
 --
--- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
-CREATE TABLE schema_migrations (
+CREATE TABLE public.schema_migrations (
     version character varying NOT NULL
 );
 
@@ -221,7 +218,7 @@ CREATE TABLE schema_migrations (
 -- Name: submission_events_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
-CREATE SEQUENCE submission_events_id_seq
+CREATE SEQUENCE public.submission_events_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -233,14 +230,14 @@ CREATE SEQUENCE submission_events_id_seq
 -- Name: submission_events_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
-ALTER SEQUENCE submission_events_id_seq OWNED BY submission_events.id;
+ALTER SEQUENCE public.submission_events_id_seq OWNED BY public.submission_events.id;
 
 
 --
 -- Name: submissions_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
-CREATE SEQUENCE submissions_id_seq
+CREATE SEQUENCE public.submissions_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -252,14 +249,14 @@ CREATE SEQUENCE submissions_id_seq
 -- Name: submissions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
-ALTER SEQUENCE submissions_id_seq OWNED BY submissions.id;
+ALTER SEQUENCE public.submissions_id_seq OWNED BY public.submissions.id;
 
 
 --
--- Name: users; Type: TABLE; Schema: public; Owner: -
+-- Name: users; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
-CREATE TABLE users (
+CREATE TABLE public.users (
     id bigint NOT NULL,
     name character varying,
     email character varying,
@@ -274,7 +271,7 @@ CREATE TABLE users (
 -- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
-CREATE SEQUENCE users_id_seq
+CREATE SEQUENCE public.users_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -286,172 +283,172 @@ CREATE SEQUENCE users_id_seq
 -- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
-ALTER SEQUENCE users_id_seq OWNED BY users.id;
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
--- Name: articles id; Type: DEFAULT; Schema: public; Owner: -
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY articles ALTER COLUMN id SET DEFAULT nextval('articles_id_seq'::regclass);
-
-
---
--- Name: documents id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY documents ALTER COLUMN id SET DEFAULT nextval('documents_id_seq'::regclass);
+ALTER TABLE ONLY public.articles ALTER COLUMN id SET DEFAULT nextval('public.articles_id_seq'::regclass);
 
 
 --
--- Name: journals id; Type: DEFAULT; Schema: public; Owner: -
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY journals ALTER COLUMN id SET DEFAULT nextval('journals_id_seq'::regclass);
-
-
---
--- Name: submission_events id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY submission_events ALTER COLUMN id SET DEFAULT nextval('submission_events_id_seq'::regclass);
+ALTER TABLE ONLY public.documents ALTER COLUMN id SET DEFAULT nextval('public.documents_id_seq'::regclass);
 
 
 --
--- Name: submissions id; Type: DEFAULT; Schema: public; Owner: -
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY submissions ALTER COLUMN id SET DEFAULT nextval('submissions_id_seq'::regclass);
-
-
---
--- Name: users id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
+ALTER TABLE ONLY public.journals ALTER COLUMN id SET DEFAULT nextval('public.journals_id_seq'::regclass);
 
 
 --
--- Name: ar_internal_metadata ar_internal_metadata_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY ar_internal_metadata
+ALTER TABLE ONLY public.submission_events ALTER COLUMN id SET DEFAULT nextval('public.submission_events_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.submissions ALTER COLUMN id SET DEFAULT nextval('public.submissions_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Name: ar_internal_metadata_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY public.ar_internal_metadata
     ADD CONSTRAINT ar_internal_metadata_pkey PRIMARY KEY (key);
 
 
 --
--- Name: articles articles_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: articles_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
-ALTER TABLE ONLY articles
+ALTER TABLE ONLY public.articles
     ADD CONSTRAINT articles_pkey PRIMARY KEY (id);
 
 
 --
--- Name: documents documents_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: documents_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
-ALTER TABLE ONLY documents
+ALTER TABLE ONLY public.documents
     ADD CONSTRAINT documents_pkey PRIMARY KEY (id);
 
 
 --
--- Name: journals journals_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: journals_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
-ALTER TABLE ONLY journals
+ALTER TABLE ONLY public.journals
     ADD CONSTRAINT journals_pkey PRIMARY KEY (id);
 
 
 --
--- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
-ALTER TABLE ONLY schema_migrations
+ALTER TABLE ONLY public.schema_migrations
     ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
 
 
 --
--- Name: submission_events submission_events_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: submission_events_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
-ALTER TABLE ONLY submission_events
+ALTER TABLE ONLY public.submission_events
     ADD CONSTRAINT submission_events_pkey PRIMARY KEY (id);
 
 
 --
--- Name: submissions submissions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: submissions_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
-ALTER TABLE ONLY submissions
+ALTER TABLE ONLY public.submissions
     ADD CONSTRAINT submissions_pkey PRIMARY KEY (id);
 
 
 --
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
-ALTER TABLE ONLY users
+ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
 
 --
--- Name: index_articles_on_user_id; Type: INDEX; Schema: public; Owner: -
+-- Name: index_articles_on_user_id; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
-CREATE INDEX index_articles_on_user_id ON articles USING btree (user_id);
-
-
---
--- Name: index_submission_events_on_submission_id; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX index_submission_events_on_submission_id ON submission_events USING btree (submission_id);
+CREATE INDEX index_articles_on_user_id ON public.articles USING btree (user_id);
 
 
 --
--- Name: index_submissions_on_article_id; Type: INDEX; Schema: public; Owner: -
+-- Name: index_submission_events_on_submission_id; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
-CREATE INDEX index_submissions_on_article_id ON submissions USING btree (article_id);
-
-
---
--- Name: index_submissions_on_journal_id; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX index_submissions_on_journal_id ON submissions USING btree (journal_id);
+CREATE INDEX index_submission_events_on_submission_id ON public.submission_events USING btree (submission_id);
 
 
 --
--- Name: submissions fk_rails_1da1d79383; Type: FK CONSTRAINT; Schema: public; Owner: -
+-- Name: index_submissions_on_article_id; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
-ALTER TABLE ONLY submissions
-    ADD CONSTRAINT fk_rails_1da1d79383 FOREIGN KEY (journal_id) REFERENCES journals(id);
-
-
---
--- Name: articles fk_rails_3d31dad1cc; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY articles
-    ADD CONSTRAINT fk_rails_3d31dad1cc FOREIGN KEY (user_id) REFERENCES users(id);
+CREATE INDEX index_submissions_on_article_id ON public.submissions USING btree (article_id);
 
 
 --
--- Name: submissions fk_rails_a05ac76e56; Type: FK CONSTRAINT; Schema: public; Owner: -
+-- Name: index_submissions_on_journal_id; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
-ALTER TABLE ONLY submissions
-    ADD CONSTRAINT fk_rails_a05ac76e56 FOREIGN KEY (article_id) REFERENCES articles(id);
+CREATE INDEX index_submissions_on_journal_id ON public.submissions USING btree (journal_id);
+
+
+--
+-- Name: fk_rails_1da1d79383; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.submissions
+    ADD CONSTRAINT fk_rails_1da1d79383 FOREIGN KEY (journal_id) REFERENCES public.journals(id);
+
+
+--
+-- Name: fk_rails_3d31dad1cc; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.articles
+    ADD CONSTRAINT fk_rails_3d31dad1cc FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- Name: fk_rails_a05ac76e56; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.submissions
+    ADD CONSTRAINT fk_rails_a05ac76e56 FOREIGN KEY (article_id) REFERENCES public.articles(id);
 
 
 --
 -- PostgreSQL database dump complete
 --
 
-SET search_path TO "$user", public;
+SET search_path TO "$user",public;
 
 INSERT INTO "schema_migrations" (version) VALUES
 ('20171201012107'),
